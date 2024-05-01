aee0e67fb8248687bce7092a06043d05842ef6ec
doha issam chehata <168630365+DohaKhalil@users.noreply.github.com>
2024-05-01 11:27:59 +0200

    Add files via upload

---
+++ DataContext.cs
@@ -0,0 +1,31 @@
+﻿using EFSossion2.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFSossion2.Context
+{
+    public class DataContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-T3NIN6L\\SQLEXPRESS;Database=EFssession2;Trusted_Connection=True;");
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().HasOne<Department>().WithOne(M => M.Manager).HasForeignKey<Department>(d=> d.EmpId);
+            modelBuilder.Entity<Employee>().HasOne<Department>().WithMany(c => c.Employee);
+
+
+        }
+
+    }
+
+}
---
+++ Migrations/20240220082613_OneTwoRelations.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFSossion2.Migrations
+{
+    public partial class OneTwoRelations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DeoId",
+                table: "Employees",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "DepartmentId",
+                table: "Employees",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "DepartmentId1",
+                table: "Employees",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ManegeId",
+                table: "Employees",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ManegerId",
+                table: "Departments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_DepartmentId",
+                table: "Employees",
+                column: "DepartmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_DepartmentId1",
+                table: "Employees",
+                column: "DepartmentId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_ManegeId",
+                table: "Employees",
+                column: "ManegeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Departments_DepartmentId",
+                table: "Employees",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Departments_DepartmentId1",
+                table: "Employees",
+                column: "DepartmentId1",
+                principalTable: "Departments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Departments_ManegeId",
+                table: "Employees",
+                column: "ManegeId",
+                principalTable: "Departments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Departments_DepartmentId",
+                table: "Employees");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Departments_DepartmentId1",
+                table: "Employees");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Departments_ManegeId",
+                table: "Employees");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Employees_DepartmentId",
+                table: "Employees");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Employees_DepartmentId1",
+                table: "Employees");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Employees_ManegeId",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "DeoId",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "DepartmentId",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "DepartmentId1",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "ManegeId",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "ManegerId",
+                table: "Departments");
+        }
+    }
+}
